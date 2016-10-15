633565cbd0237d5871ff081f85ef3b686af8e56e
Abhishek Jain <ajj_2396@yahoo.com>
2016-10-15 23:42:52 +0530

    Add files via upload

---
+++ even_odd.java
@@ -0,0 +1,15 @@
+public class even_odd
+{
+	public static void main(String args[])
+	{
+		int a = 2;
+		if (a%2==0)
+		{
+			System.out.println("Even!");
+		}
+		else
+		{
+			System.out.println("Odd!");
+		}
+	}
+}
