c3c45c1d18d4744f983090b6306dd8c180ea8572
Abhishek Jain <ajj_2396@yahoo.com>
2016-11-09 00:28:56 +0530

    Upload pattern 3

---
+++ pattern3.java
@@ -0,0 +1,28 @@
+/*
+This program will print the following patter
+
+    *
+   **
+  ***
+ ****
+*****
+
+ */
+public class pattern3
+{
+	public static void main(String args[])
+	{
+		int count =0;
+		for (int row=0;row<=5;row++)
+		{
+			for (int space=0; space<(5-row); space++ ) {
+				System.out.print(" ");
+			}
+			for (int col=0;col<row;col++)
+			{
+				System.out.print("$");
+			}
+			System.out.println();
+		}
+	}
+}
